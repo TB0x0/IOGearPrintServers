e62c4fe94813b8d9a6aa1320ff14212784f7f610
Thomas <tmclean1212@gmail.com>
2022-01-05 17:57:59 -0600

    added debugging messages fro resetio script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IOGearPrintServers
-IOGear print server utilities
+Repository containing IOGear Print Server Utilities
+
+### Example
+`python ioscriptname.py argument1 argument2`
+
+A quirk of the python telnet package is that all strings must be converted to binary strings. I accomplished this by using the encode() function.
--- resetiogear.py
+++ resetiogear.py
@@ -9,19 +9,23 @@ def resetIOgear(arglength, argcontent):
     content = str(argcontent)[1:-1]
     connections = 1
     while (connections < arglength):
-        tel = telnetlib.Telnet(content.split(',')[connections], 23, 3)
-        tel.read_until("Password: ")
-        tel.write("\n")
+        current_host = content.split(',')[connections]
+        tel = telnetlib.Telnet(current_host.encode('utf-8'))
+        print ("Logging onto device")
+        tel.read_until('Password:'.encode('utf-8'))
+        tel.write('\r\n'.encode('utf-8'))
+        
+        print ("Resetting Device")
+        tel.read_until('->'.encode('utf-8'))
+        tel.write('4\r\n'.encode('utf-8'))
 
-        tel.read_until("-> ")
-        tel.write("4\n")
+        tel.read_until('N)'.encode('utf-8'))
+        tel.write('Y\r\n'.encode('utf-8'))
 
-        tel.read_until("(Y/N) ")
-        tel.write("Y")
+        print ("Device Reset Complete")
         connections += 1
 
 
 # Pass command line arguments to function
 resetIOgear(len(sys.argv), str(sys.argv))
 
-      

3bbd73e24b3f3ccb6b018fa2c552ad36d4fa166e
Thomas <tmclean1212@gmail.com>
2022-01-10 11:49:38 -0600

    added prepareHostString function

--- resetiogear.py
+++ resetiogear.py
@@ -2,15 +2,60 @@
 # Thomas McLean 2022
 
 import sys
+import time
 import telnetlib
 
+def prepareHostString(host_string, selection):
+    host_string = host_string.split(',')[selection].strip()
+    if host_string.startswith('\'') and host_string.endswith('\''):
+        prepared_string = host_string[1:-1]
+
+    return prepared_string
+
+def logOutput(output_string):
+    open_file = open('IOGearLogfile.log', 'a')
+    open_file.write("\n" + output_string)
+
+    open_file.close()
+
+def checkIOgearStatus(arglength, argcontent):
+    content = str(argcontent)[1:-1]
+    connections = 1
+    while (connections < arglength):
+        p_host = prepareHostString(content, connections)
+        print(p_host.encode('utf-8'))
+
+        tel = telnetlib.Telnet(p_host.encode('utf-8'))
+        print ("Logging onto device")
+        tel.read_until('Password:'.encode('utf-8'))
+        tel.write('\r\n'.encode('utf-8'))
+
+        print ("Querying print server status")
+        tel.read_until('->'.encode('utf-8'))
+        tel.write('1\r\n'.encode('utf-8'))
+
+        print ("Querying connected printers")
+        tel.read_until('->'.encode('utf-8'))
+        tel.write('2\r\n'.encode('utf-8'))
+
+        print ("Getting printer status")
+        time.sleep(5)
+        log_txt = tel.read_very_eager()
+        print (log_txt)
+
+        tel.close()
+        connections += 1
+
+        
 # Take a list of connections, step through them, and input commands
 def resetIOgear(arglength, argcontent):
     content = str(argcontent)[1:-1]
     connections = 1
     while (connections < arglength):
-        current_host = content.split(',')[connections]
-        tel = telnetlib.Telnet(current_host.encode('utf-8'))
+        p_host = prepareHostString(content, connections)
+        print(p_host.encode('utf-8'))
+
+        tel = telnetlib.Telnet(p_host.encode('utf-8'))
         print ("Logging onto device")
         tel.read_until('Password:'.encode('utf-8'))
         tel.write('\r\n'.encode('utf-8'))
@@ -28,4 +73,4 @@ def resetIOgear(arglength, argcontent):
 
 # Pass command line arguments to function
 resetIOgear(len(sys.argv), str(sys.argv))
-
+#checkIOgearStatus(len(sys.argv), str(sys.argv))
